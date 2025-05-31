5bfa7269131d0b1e100c374ee2394062af2fe491
Thuy Anh Nguyen <thng0033@uni.sydney.edu,au>
2025-05-31 13:01:01 +1000

    Update env.

--- api/db.php
+++ api/db.php
@@ -1,5 +1,6 @@
 $db = new PDO(
-  'pgsql:host=' . getenv('DB_HOST') . ';port=5432;dbname=' . getenv('DB_NAME'),
+  'pgsql:host=' . getenv('DB_HOST') . ';port=' . getenv('DB_PORT') . ';dbname=' . getenv('DB_NAME'),
   getenv('DB_USER'),
   getenv('DB_PASSWORD')
 );
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
